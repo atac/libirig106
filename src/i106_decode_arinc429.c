e0fef4c86c9196dfc4748562b2581c9e13f19bcc
Micah Ferrill <mcferrill@gmail.com>
2017-06-22 12:34:02 -0500

    Finished decode_arinc429

--- src/i106_decode_arinc429.c
+++ src/i106_decode_arinc429.c
@@ -1,12 +1,8 @@
 /****************************************************************************
 
- i106_decode_arinc429.c -
+ i106_decode_arinc429.c
 
  ****************************************************************************/
-
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
 
 #include "config.h"
 #include "stdint.h"
@@ -15,311 +11,51 @@
 #include "i106_time.h"
 #include "i106_decode_arinc429.h"
 
-// #include <windows.h>
 
-#ifdef __cplusplus
-namespace Irig106 {
-#endif
+/* Function Declaration */
 
 
-/*
- * Macros and definitions
- * ----------------------
- */
-
-
-
-/*
- * Data structures
- * ---------------
- */
-
-#if 0
-typedef struct
-    {
-    uint32_t hour;
-    uint32_t minute;
-    uint32_t second;
-    uint32_t msecond;
-    uint32_t usecond;
-    } currentWordTime;
-#endif
-
-/*
- * Module data
- * -----------
- */
-
-
-
-/*
- * Function Declaration
- * --------------------
- */
-
-
-
-/* ======================================================================= */
-
-#if 0
-EnI106Status I106_CALL_DECL
-enI106_Decode_Arinc429F0(SuI106Ch10Header       * psuHeader,
-                        void                    * pvBuff,
-                        SuArinc429F0_CurrMsg    * psuCurrMsg)
-    {
-    int i;
-    int numDataWords;
-    SuArinc429F0_Header *tmpPtrH;
-    SuArinc429F0_Data   *tmpPtrD;
-
+I106Status I106_Decode_FirstArinc429F0(I106C10Header *header, void * buffer, Arinc429F0_Message * msg){
     // Save pointer to channel specific data
-    psuCurrMsg->psuChanSpec = (SuArinc429F0_ChanSpec *)pvBuff;
+    msg->CSDW = (Arinc429F0_CSDW *)buffer;
 
-    numDataWords = psuCurrMsg->psuChanSpec->uMsgCount;
-
-    psuCurrMsg->uMsgNum = numDataWords;
-
-    // Read in the rest of the data, alternating intra packet headers and data words
-    psuCurrMsg->ulCurrOffset = sizeof(SuArinc429F0_ChanSpec);
-
-//    psuCurrMsg->psu429Hdr  = new SuArinc429F0_Header[numDataWords];
-//    psuCurrMsg->psu429Data = new SuArinc429F0_Data[numDataWords];
-
-    for (i = 0; i < numDataWords; i++) 
-        {
-        // Check to make sure the data does run beyond the end of the buffer
-        if (psuCurrMsg->ulCurrOffset > psuHeader->ulDataLen)
-            return I106_BUFFER_OVERRUN;
-
-        tmpPtrH = (SuArinc429F0_Header*)((char*)pvBuff + psuCurrMsg->ulCurrOffset);
-        psuCurrMsg->psu429Hdr[i] = *tmpPtrH;
-        psuCurrMsg->ulCurrOffset += sizeof(SuArinc429F0_Header);
-
-        // Check to make sure the data does run beyond the end of the buffer
-        if (psuCurrMsg->ulCurrOffset > psuHeader->ulDataLen)
-            return I106_BUFFER_OVERRUN;
-
-        tmpPtrD = (SuArinc429F0_Data*)((char*)pvBuff + psuCurrMsg->ulCurrOffset);
-        psuCurrMsg->psu429Data[i] = *tmpPtrD;
-        psuCurrMsg->ulCurrOffset += sizeof(SuArinc429F0_Data);
-        } // end for all data words
-
-    return I106_OK;
-    }
-#endif
-
-
-
-/* ----------------------------------------------------------------------- */
-
-EnI106Status I106_CALL_DECL 
-    enI106_Decode_FirstArinc429F0(SuI106Ch10Header * psuHeader,
-                              void                 * pvBuff,
-                              SuArinc429F0_CurrMsg * psuMsg)
-    {
-    // Save pointer to channel specific data
-    psuMsg->psuChanSpec = (SuArinc429F0_ChanSpec *)pvBuff;
-
-    psuMsg->ulCurrOffset = sizeof(SuArinc429F0_ChanSpec);
+    msg->Offset = sizeof(Arinc429F0_CSDW);
 
     // Check for no messages
-    psuMsg->uMsgNum = 0;
-    if (psuMsg->psuChanSpec->uMsgCount == 0)
+    msg->MessageNumber = 0;
+    if (msg->CSDW->Count == 0)
         return I106_NO_MORE_DATA;
 
     // Make the time for the current message
-    vTimeArray2LLInt(psuHeader->aubyRefTime, &(psuMsg->llIntPktTime));
+    TimeArray2LLInt(header->RTC, &(msg->IPTS));
 
     // Set pointers the header and data
-    psuMsg->psu429Hdr  = (SuArinc429F0_Header *)((char *)pvBuff + sizeof(SuArinc429F0_ChanSpec));
-    psuMsg->psu429Data = (SuArinc429F0_Data *)((char *)(psuMsg->psu429Hdr) + sizeof(SuArinc429F0_Header));
+    msg->IPH  = (Arinc429F0_IPH *)((char *)buffer + sizeof(Arinc429F0_CSDW));
+    msg->Data = (Arinc429F0_Data *)((char *)(msg->IPH) + sizeof(Arinc429F0_IPH));
 
     return I106_OK;
-    }
+}
 
 
-
-/* ----------------------------------------------------------------------- */
-
-EnI106Status I106_CALL_DECL 
-    enI106_Decode_NextArinc429F0(SuArinc429F0_CurrMsg * psuMsg)
-    {
+I106Status I106_Decode_NextArinc429F0(Arinc429F0_Message *msg){
 
     // Check for no more messages
-    psuMsg->uMsgNum++;
-    if (psuMsg->uMsgNum >= psuMsg->psuChanSpec->uMsgCount)
+    msg->MessageNumber++;
+    if (msg->MessageNumber >= msg->CSDW->Count)
         return I106_NO_MORE_DATA;
 
     // Figure out the offset to the next ARINC 429 message and
     // make sure it isn't beyond the end of the data buffer
-    psuMsg->ulCurrOffset += sizeof(SuArinc429F0_Header) + 
-                            sizeof(SuArinc429F0_Data);
-
-//    if (psuMsg->ulCurrOffset >= psuMsg->ulDataLen)
-//        return I106_BUFFER_OVERRUN;
+    msg->Offset += sizeof(Arinc429F0_IPH) + sizeof(Arinc429F0_Data);
 
     // Set pointer to the next ARINC 429 header
-    psuMsg->psu429Hdr = (SuArinc429F0_Header *)((char *)psuMsg->psu429Hdr           +
-                                                        sizeof(SuArinc429F0_Header) + 
-                                                        sizeof(SuArinc429F0_Data));
+    msg->IPH = (Arinc429F0_IPH *)((char *)msg->IPH + sizeof(Arinc429F0_IPH) + sizeof(Arinc429F0_Data));
 
     // Set pointer to the next ARINC 429 data buffer
-    psuMsg->psu429Data = (SuArinc429F0_Data *)((char *)(psuMsg->psu429Hdr) + sizeof(SuArinc429F0_Header));
+    msg->Data = (Arinc429F0_Data *)((char *)(msg->IPH) + sizeof(Arinc429F0_IPH));
 
     // Make the time for the current message
-    psuMsg->llIntPktTime += psuMsg->psu429Hdr->uGapTime;
+    msg->IPTS += msg->IPH->GapTime;
 
     return I106_OK;
-    }
-
-
-
-/* ----------------------------------------------------------------------- */
-
-#if 0
-EnI106Status I106_CALL_DECL
-enI106_getWordTime_Arinc429F0(int                   m_iI106Handle, 
-                              SuI106Ch10Header    * suI106Hdr, 
-                              SuArinc429F0_Header * suCurrHdrF0, 
-                              currentWordTime     * wordTime, 
-                              char                * szTime)
-    {
-    SuIrig106Time             suTime;
-    int                       hr;
-    int                       min;
-    int                       sec;
-    int                       msec;
-    int                       usec;
-    char                    * pch;
-
-    char                      HH[10]; // hour
-    char                      MM[10]; // minute
-    char                      SS[10]; // second
-    char                      MS[10]; // millisecond
-    char                      US[10]; // microsecond
-
-
-    // Is this this first data word in the packet?
-    if (wordTime->hour == -1) 
-        {
-        EnI106Status enStatus = enI106_Rel2IrigTime(m_iI106Handle, suI106Hdr->aubyRefTime, &suTime);
-        if (enStatus != I106_OK) 
-            {
-            strcpy(szTime,"HH:MM:SS:mmS:uuS");
-            return I106_TIME_NOT_FOUND;
-            }
-
-        time_t dummyTime = (time_t)suTime.ulSecs;
-        char *szTime_tmp = ctime(&dummyTime);
-        strcpy(szTime,szTime_tmp);
-        szTime[19] = '\0';
-        
-        // Parse the "szTime" string for hour, minute, and second
-        char *justTimeNumbers = &szTime[11];
-        pch = strtok(justTimeNumbers,":");
-        hr  = atoi(pch);
-        pch = strtok(NULL,":");
-        min = atoi(pch);
-        pch = strtok(NULL,"");
-        sec = atoi(pch);
-
-        // Calculate milliseconds and microseconds
-        msec = (int)(suTime.ulFrac / 10000.0);
-        usec = (int)(1000*((suTime.ulFrac / 10000.0) - (int)(suTime.ulFrac / 10000.0)));
-        
-        // Update wordTime (in case you want to use the numbers, not the string, outside of this function)
-        wordTime->hour    = hr;
-        wordTime->minute  = min;
-        wordTime->second  = sec;
-        wordTime->msecond = msec;
-        wordTime->usecond = usec;
-
-        itoa(hr,  HH,10);
-        itoa(min, MM,10);
-        itoa(sec, SS,10);
-        itoa(msec,MS,10);
-        itoa(usec,US,10);
-
-        strcpy(szTime,"");
-        strcat(szTime,HH);
-        strcat(szTime,":");
-        strcat(szTime,MM);
-        strcat(szTime,":");
-        strcat(szTime,SS);
-        strcat(szTime,":");
-        strcat(szTime,MS);
-        strcat(szTime,":");
-        strcat(szTime,US);
-        } // end if first data word
-
-    // Not first data word
-    else 
-        {
-        char szTime_tmp[80];
-        strcpy(szTime_tmp,szTime);
-        pch  = strtok(szTime_tmp,":");
-        hr   = atoi(pch);
-        pch  = strtok(NULL,":");
-        min  = atoi(pch);
-        pch  = strtok(NULL,":");
-        sec  = atoi(pch);
-        pch  = strtok(NULL,":");
-        msec = atoi(pch);
-        pch  = strtok(NULL,"");
-        usec = atoi(pch);
-
-        int gapTimeInMicroSecs = (int)(0.1*(float)suCurrHdrF0->uGapTime);
-        
-        usec += gapTimeInMicroSecs;
-        if (usec > 999) 
-            {
-            usec %= 1000;
-            msec++;
-            if (msec > 999) 
-                {
-                msec %= 1000;
-                sec++;
-                if (sec > 59) 
-                    {
-                    sec %= 60;
-                    min++;
-                    if (min > 59) 
-                        {
-                        min %= 60;
-                        hr++;
-                        if (hr > 23) 
-                            {
-                            hr %= 24;
-                            }
-                        }
-                    }
-                }
-            }
-
-        itoa(hr,  HH,10);
-        itoa(min, MM,10);
-        itoa(sec, SS,10);
-        itoa(msec,MS,10);
-        itoa(usec,US,10);
-
-        strcpy(szTime,"");
-        strcat(szTime,HH);
-        strcat(szTime,":");
-        strcat(szTime,MM);
-        strcat(szTime,":");
-        strcat(szTime,SS);
-        strcat(szTime,":");
-        strcat(szTime,MS);
-        strcat(szTime,":");
-        strcat(szTime,US);
-        } // end if not first data word
-
-    return I106_OK;
-    }
-#endif
-
-
-#ifdef __cplusplus
 }
-#endif
-
--- src/i106_decode_arinc429.h
+++ src/i106_decode_arinc429.h
@@ -1,6 +1,6 @@
 /****************************************************************************
 
- i106_decode_arinc429.h - 
+ i106_decode_arinc429.h
  Created by Bob Baggerman
 
  ****************************************************************************/
@@ -8,110 +8,61 @@
 #ifndef _I106_DECODE_ARINC429_H
 #define _I106_DECODE_ARINC429_H
 
-#ifdef __cplusplus
-namespace Irig106 {
-extern "C" {
-#endif
 
-
-/*
- * Macros and definitions
- * ----------------------
- */
-
-
-/*
- * Data structures
- * ---------------
- */
+/* Data structures */
 
 #if defined(_MSC_VER)
 #pragma pack(push,1)
 #endif
 
 // Channel specific data word
-// --------------------------
-
-typedef struct Arinc429F0_ChanSpec_S
-    {
-    uint32_t    uMsgCount       : 16;      // Message count
-    uint32_t    Reserved        : 16;      //
-#if !defined(__GNUC__)
-    } SuArinc429F0_ChanSpec;
-#else
-    } __attribute__ ((packed)) SuArinc429F0_ChanSpec;
-#endif
+typedef struct Arinc429F0_CSDW Arinc429F0_CSDW;
+struct Arinc429F0_CSDW {
+    uint32_t    Count     : 16;      // Message count
+    uint32_t    Reserved  : 16;
+} PACKED;
 
 // Intra-message header
-typedef struct Arinc429F0_Header_S
-    {
-    uint32_t    uGapTime        : 20;      // Gap Time
-    uint32_t    Reserved        :  1;      // 
-    uint32_t    uBusSpeed       :  1;      // Bus Speed
-    uint32_t    bParityError    :  1;      // Parity Error
-    uint32_t    bFormatError    :  1;      // Data type
-    uint32_t    uBusNum         :  8;      // Bus number
-#if !defined(__GNUC__)
-    } SuArinc429F0_Header;
-#else
-    } __attribute__ ((packed)) SuArinc429F0_Header;
-#endif
+typedef struct Arinc429F0_IPH Arinc429F0_IPH;
+struct Arinc429F0_IPH {
+    uint32_t    GapTime        : 20;
+    uint32_t    Reserved       :  1; 
+    uint32_t    BusSpeed       :  1;
+    uint32_t    ParityError    :  1;
+    uint32_t    FormatError    :  1;
+    uint32_t    BusNumber      :  8;
+} PACKED;
 
 
 // ARINC 429 data format
-typedef struct Arinc429F0_Data_S
-    {
-    uint32_t    uLabel          :  8;      // Label
-    uint32_t    uSDI            :  2;      // Source/Destination Identifiers
-    uint32_t    uData           : 19;      // Data
-    uint32_t    uSSM            :  2;      // Sign/Status Matrix
-    uint32_t    uParity         :  1;      // Parity
-#if !defined(__GNUC__)
-    } SuArinc429F0_Data;
-#else
-    } __attribute__ ((packed)) SuArinc429F0_Data;
-#endif
+typedef struct Arinc429F0_Data Arinc429F0_Data;
+struct Arinc429F0_Data {
+    uint32_t    Label          :  8;
+    uint32_t    SDI            :  2;      // Source/Destination Identifiers
+    uint32_t    Data           : 19;
+    uint32_t    SSM            :  2;      // Sign/Status Matrix
+    uint32_t    Parity         :  1;
+} PACKED;
 
 
 // Current ARINC 429 message
-typedef struct
-    {
-    unsigned int            uMsgNum;
-    uint32_t                ulCurrOffset;   // Offset into data buffer
-    SuArinc429F0_ChanSpec * psuChanSpec;
-    int64_t                 llIntPktTime;   // Intrapacket message relative time
-    SuArinc429F0_Header   * psu429Hdr;      // Pointer to the 429 header
-    SuArinc429F0_Data     * psu429Data;     // Pointer to the 429 data
-#if !defined(__GNUC__)
-    } SuArinc429F0_CurrMsg;
-#else
-    } __attribute__ ((packed)) SuArinc429F0_CurrMsg;
-#endif
-
+typedef struct {
+    unsigned int       MessageNumber;
+    uint32_t           Offset;
+    Arinc429F0_CSDW  * CSDW;
+    int64_t            IPTS;
+    Arinc429F0_IPH   * IPH;
+    Arinc429F0_Data  * Data;
+} PACKED Arinc429F0_Message;
 
 
 #if defined(_MSC_VER)
 #pragma pack(pop)
 #endif
 
-/*
- * Function Declaration
- * --------------------
- */
 
-EnI106Status I106_CALL_DECL 
-    enI106_Decode_FirstArinc429F0(SuI106Ch10Header * psuHeader,
-                              void                 * pvBuff,
-                              SuArinc429F0_CurrMsg * psuMsg);
-
-EnI106Status I106_CALL_DECL 
-    enI106_Decode_NextArinc429F0(SuArinc429F0_CurrMsg * psuMsg);
-
-
-
-#ifdef __cplusplus
-}
-}
-#endif
+/* Function Declaration */
+I106Status I106_Decode_FirstArinc429F0(I106C10Header *header, void *buffer, Arinc429F0_Message *msg);
+I106Status I106_Decode_NextArinc429F0(Arinc429F0_Message *msg);
 
 #endif
