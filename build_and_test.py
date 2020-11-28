69be9f3d141bbe693e01c95ef59746bdda9a69f6
Micah Ferrill <mcferrill@gmail.com>
2020-11-28 15:28:30 -0600

    Build system updates.

--- build_and_test.py
+++ build_and_test.py
@@ -5,19 +5,18 @@ import sys
 
 if 'rebuild' in sys.argv:
     if sys.platform == 'win32':
-        os.system('del /F /Q build && mkdir build')
+        os.system('del /F /Q build')
     else:
-        os.system('rm -rf build && mkdir build')
+        os.system('rm -rf build')
 
+os.system('mkdir build')
+os.chdir('build')
+os.system('cmake ..')
 if sys.platform == 'win32':
-    os.chdir('build')
-    os.system('cmake ..')
     os.system('cmake --build . --config Release')
     os.chdir('..')
     os.system('.\\build\\Release\\test_runner.exe')
 else:
-    os.chdir('build')
-    os.system('cmake ..')
     os.system('make')
     os.chdir('..')
     os.system('./build/test_runner')
