a0d9a1d1da5f6adc091fa09d50d5b816f75597ae
Micah Ferrill <mcferrill@gmail.com>
2021-02-01 16:09:46 -0600

    Minor edits.

--- build_and_test.py
+++ build_and_test.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python3
 
 import os
+import shutil
 import sys
+
+windows = sys.platform == 'win32'
 
 
 def run(cmd):
     status = os.system(cmd)
-    if sys.platform != 'win32' and status:
+    if status and not windows:
         print('%s failed: %s' % (cmd, status))
         sys.exit(status)
 
 
+# Ensure build directory exists (recreate if requested)
 if 'rebuild' in sys.argv:
-    if sys.platform == 'win32':
-        run('del /F /Q build')
-    else:
-        run('rm -rf build')
-
+    shutil.rmtree('build')
 if not os.path.exists('build'):
     run('mkdir build')
+
+# Run cmake
 os.chdir('build')
 run('cmake ..')
-if sys.platform == 'win32':
-    run('cmake --build . --config Release')
-    os.chdir('..')
-    if not os.path.exists('.\\build\\Release\\test_runner.exe'):
-        print('Test runner not found!')
-        import pprint
-        pprint.pprint(os.walk('build'))
-    run('.\\build\\Release\\test_runner.exe')
-else:
-    run('make')
-    os.chdir('..')
-    run('./build/test_runner')
+
+# Run platform-specific build command
+run('cmake --build . --config Release' if windows else 'make')
+os.chdir('..')
+
+# Run test suite (must be from root project directory due to test file paths)
+test_runner = './build/test_runner'
+if windows:
+    test_runner = '.\\build\\Release\\test_runner.exe'
+if not os.path.exists(test_runner):
+    print('Test runner not found!')
+run(test_runner)
